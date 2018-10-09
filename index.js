c55908a56eabd72174e8fb7f674639a854b7c01e
Jeremy Greer <jex.grizzle@gmail.com>
2018-10-09 10:43:10 -0400

    Move extractor to own file

--- index.js
+++ index.js
@@ -2,59 +2,7 @@
 
 const fs = require('fs')
 const path = require('path')
-
-
-// Find lines with timestamp.
-const getTimes = (text) => {
-  const times = []
-  const regex = (/^\w{3} \w{3}\s+ \d{1,2} (\d{2}:\d{2}:\d{2}) \w{3} \d{4} (.+)/gm)
-  let matches = regex.exec(text)
-  while (matches) {
-    const time = matches[1]
-    const description = matches[2]
-    times.push({
-      time,
-      description,
-    })
-    matches = regex.exec(text)
-  }
-
-  return times
-}
-
-const isEndTime = (time) => {
-  return time.description === 'stop'
-}
-
-// returns minutes
-const subtractTimes = (end, start) => {
-  const endParts = end.split(':').map(x => parseInt(x, 10))
-  const startParts = start.split(':').map(x => parseInt(x, 10))
-  const hours = endParts[0] - startParts[0]
-  const minutes = endParts[1] - startParts[1]
-  return minutes + hours * 60
-}
-
-const getTimeRanges = (times) => {
-  return times.reduce((accumulator, time) => {
-    if (isEndTime(time)) {
-      const previous = accumulator[accumulator.length - 1]
-      previous.end = time.time
-      previous.durationMinutes = subtractTimes(previous.end, previous.start)
-    } else {
-      accumulator.push({
-        description: time.description,
-        start: time.time,
-      })
-    }
-    return accumulator
-  }, [])
-}
-
-const extractTimeRanges = (text) => {
-  const times = getTimes(text)
-  return getTimeRanges(times)
-}
+const extractTimeRanges = require('./extractor')
 
 if (require.main === module) {
   let file
